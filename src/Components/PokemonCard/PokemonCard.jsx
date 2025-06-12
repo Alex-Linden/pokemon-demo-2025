cc193a9359e85219778c1ce1db782ca03a4c27e7
Alex Linden <alextl2004@gmail.com>
2025-06-12 12:10:07 -0700

    cards dynamically showing info

--- src/Components/PokemonCard/PokemonCard.jsx
+++ src/Components/PokemonCard/PokemonCard.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-//  "id": 4,
-//     "name": "Pikachu",
-//     "type": "Electric",
-//     "description": "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.",
-//     "imageUrl": "https://img.pokemondb.net/artwork/large/pikachu.jpg"
 
-const PokemonCard = () => {
+const PokemonCard = (props) => {
     return (
         <>
             <fieldset>
                 <legend>PokemonCard.jsx</legend>
                 <div className="card">
-                    <img src="https://img.pokemondb.net/artwork/large/pikachu.jpg" alt="pikachu" />
-                    <h2>Pikachu</h2>
-                    <h4>Type: Electric</h4>
-                    <p>Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.</p>
+                    <img src={props.imageUrl} alt={props.name} />
+                    <h2>{props.name}</h2>
+                    <h4>Type: {props.type}</h4>
+                    <p>{props.description}</p>
                 </div>
             </fieldset>
         </>
